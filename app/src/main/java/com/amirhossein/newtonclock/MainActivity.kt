993f8311281c833953a12c2ca599b4b6e07bd054
amirhossein <amirhossein.jahanshahi13@gmail.com>
2023-05-01 20:19:24 +0330

    add clock model

--- app/src/main/java/com/amirhossein/newtonclock/MainActivity.kt
+++ app/src/main/java/com/amirhossein/newtonclock/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,14 +17,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewtonClockTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
 
                 }
             }
         }
     }
 }
-}
+
 
 @Preview(showBackground = true)
 @Composable
---
+++ app/src/main/java/com/amirhossein/newtonclock/model/SmallClock.kt
@@ -0,0 +1,16 @@
+package com.amirhossein.newtonclock.model
+
+data class SmallClock(
+    val hourHand : Int,
+    val minuteHand : Int,
+){
+    companion object{
+        val TopRight = SmallClock(9, 6)
+        val TopLeft = SmallClock(3, 6)
+        val BottomRight = SmallClock(9, 0)
+        val BottomLeft = SmallClock(3, 0)
+        val Vertical = SmallClock(0, 6)
+        val Horizontal = SmallClock(3, 9)
+        val DEFAULT = SmallClock(7,1)
+    }
+}
